a5b6a295779c3d726325801636d67fd2c3285b8a
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-13 09:27:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+const dogName="Byron";
+const dogBreed="poodle";
+function exerciseDog(dogName, dogBreed) {
+console.log(`Wake ${dogName} the ${dogBreed}`);
+console.log(`Leash ${dogName} the ${dogBreed}`);
+console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+console.log(`Walk home with ${dogName} the ${dogBreed}`);
+console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
